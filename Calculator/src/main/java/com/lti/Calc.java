ad02869f661928667374024f7704e1f51d0a9833
Anjum-baseera <anjumsyed59@gmail.com>
2020-12-21 14:59:01 +0530

    commit 2

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -4,7 +4,11 @@ public class Calc {
 	public static int add(int num1,int num2) {
 		return num1+num2;
 	}
-	public static void main(String[] args) {
-		System.out.println(add(10,20));
+	public static int sub(int num1,int num2) {
+		return num1-num2;
 	}
+	public static void main(String[] args) {
+		System.out.println("Sum="+add(10,20));
+		System.out.println("Diff="+sub(10,20));
+	}	
 }
