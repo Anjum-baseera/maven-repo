d5cbc3d1af886b0e8f439ff669e0922c6f21e2c3
Anjum-baseera <anjumsyed59@gmail.com>
2020-12-21 15:09:50 +0530

    prod commit

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -7,8 +7,12 @@ public class Calc {
 	public static int sub(int num1,int num2) {
 		return num1-num2;
 	}
+	public static int prod(int num1,int num2) {
+		return num1*num2;
+	}
 	public static void main(String[] args) {
 		System.out.println("Sum="+add(10,20));
 		System.out.println("Diff="+sub(90,20));
+		System.out.println("Prod="+prod(20,20));
 	}	
 }
